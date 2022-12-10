3a4601a29bfd11d730b1f6c6b487a7e5fd9e19b0
Manar-01 <66436673+Manar-01@users.noreply.github.com>
2022-12-10 13:10:58 +0000

    connect with db

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = "hello_world.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'saber-1', # Change example - blog_data
+        'USER': 'admin', # Change
+        'PASSWORD': 'Bestteam27', # Change
+        'HOST': 'saber-1.cmq947vs5sx3.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
