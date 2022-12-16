76c348ee2a96e627dce09c4442f3114eed7f51da
ReemQaid <reemqaid@outlook.sa>
2022-12-16 20:35:39 +0300

    test

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('', views.signup, name='blog-signup'),
+    path('', views.signin, name='signin'),
+    path('signout', views.signout, name='signout'),
 ]
