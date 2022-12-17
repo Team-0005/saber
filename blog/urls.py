a991c7e1f339a8984d2a21b8dc73a46e4b269859
sahar siyam <108266400+s7rsiyam@users.noreply.github.com>
2022-12-17 14:48:14 +0000

    add url base

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
-    path('', views.signup, name='blog-signup'),
-    path('', views.signin, name='signin'),
-    path('signout', views.signout, name='signout'),
+    # path('', views.signup, name='blog-signup'),
+    # path('', views.signin, name='signin'),
+    # path('signout', views.signout, name='signout'),
+    path('spetestinfo/', views.test, name= 'spetestinfo'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,38 +6,39 @@ from django.contrib import messages
 # Create your views here.
 
 
-
+def test(request):
+     return render(request, 'blog/Spetestinfo.html')
 
 def home(request):
     return render(request, 'blog/home.html')  
 
-def signup(request):
-    if request.method == "post":
-        print("This is post")
-        p_email = request.post['p_email']
-        p_password = request.post['p_password']
-        p_f_name = request.post['p_f_name']
-        p_l_name = request.post['p_l_name']
-        p_phone_no = request.post['p_phone_no']
-        current_job_title = request.post['current_job_title']
-        scfhs_no = request.post['scfhs_no']
-        # scfhs_file = request.post['p_email']
-        req_status = 0
+# def signup(request):
+#     if request.method == "post":
+#         print("This is post")
+#         p_email = request.post['p_email']
+#         p_password = request.post['p_password']
+#         p_f_name = request.post['p_f_name']
+#         p_l_name = request.post['p_l_name']
+#         p_phone_no = request.post['p_phone_no']
+#         current_job_title = request.post['current_job_title']
+#         scfhs_no = request.post['scfhs_no']
+#         # scfhs_file = request.post['p_email']
+#         req_status = 0
 
-        if Psychologist.objects.filter(p_email=p_email) or Psychologist.objects.filter(scfhs_no=scfhs_no):
-            messages.error(request,"الإيميل أو رقم الرخصة مسجّل في سابر مسبقًا")
+#         if Psychologist.objects.filter(p_email=p_email) or Psychologist.objects.filter(scfhs_no=scfhs_no):
+#             messages.error(request,"الإيميل أو رقم الرخصة مسجّل في سابر مسبقًا")
             
 
-        psycho = Psychologist.objects.create_user(p_email,p_password,p_f_name ,p_l_name,p_phone_no, current_job_title,scfhs_no ,req_status)
+#         psycho = Psychologist.objects.create_user(p_email,p_password,p_f_name ,p_l_name,p_phone_no, current_job_title,scfhs_no ,req_status)
 
-        psycho.save()
+#         psycho.save()
 
-        messages.success(request,"تم إرسال الطلب بنجاح")
+#         messages.success(request,"تم إرسال الطلب بنجاح")
     
-    return render(request, 'blog/signup.html')
+#     return render(request, 'blog/signup.html')
 
-def signin(request):
-    return render(request, 'blog/home.html')
+# def signin(request):
+#     return render(request, 'blog/home.html')
 
-def signout(request):
-    pass
+# def signout(request):
+#     pass
