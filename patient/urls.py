1da4e30321094275b8bc8b72f0350bcf866b420e
Sarah aldumaiji <75248925+saronhkh@users.noreply.github.com>
2022-12-22 16:24:49 +0300

    ssss

--- patient/urls.py
+++ patient/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.list_of_patient, name='add-patient'),
+    path('', views.list_of_patient, name='patient-list'),
     # path('', views.list_of_patient, name='patient-list'),
 ]
