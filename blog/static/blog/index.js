f480d7cf1da4085d6107b4eff9528b339585dc13
Manar-01 <s439000802@st.uqu.edu.sa>
2022-12-30 03:54:23 +0300

    validate inputs

--- blog/static/blog/index.js
+++ blog/static/blog/index.js
@@ -1,8 +1,4 @@
-function check_singin() {
-  document.getElementById("massege").innerText =
-    "الرجاء تسجيل الدخول اولا أو انشئ حساب";
-}
-
+form = false;
 // Get the modal
 var modal = [
   document.getElementById("id00"),
@@ -13,25 +9,75 @@ var modal = [
 ];
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function () {
+window.onclick = function (event) {
   for (let index = 0; index < modal.length; index++) {
-    if (modal[index].style.display == "block") {
-      element.style.display = "none";
+    if (event.target == modal[index]) {
+      modal[index].style.display = "none";
     }
   }
 };
 
+function check_symbol(input_id,inputmessage) {
+  var input =  document.getElementById(input_id);
+  var regularExp =/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/
+  if (input.value.match(regularExp)){
+    input.style.borderColor='red';
+    document.getElementById(inputmessage).innerHTML="الرجاء عدم استخدام رموز مثل */+";
+    form = false;
+  }
+  else{
+    document.getElementById(inputmessage).innerHTML= null;
+    input.style.borderColor = "#5399B5";
+    form = true;
+  }
+}
 
-function check_symbol() {}
+function check_email(mail_id,mailmessage) {
+  var input =  document.getElementById(mail_id);
+  var regularExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+  if (!input.value.match(regularExp)){
+    input.style.borderColor='red';
+    document.getElementById(mailmessage).innerHTML="الرجاء التأكد من صحة البريد الالكتروني";
+  }
+  else{
+    document.getElementById(mailmessage).innerHTML= null;
+    input.style.borderColor = "#5399B5";
+    form = true;
+  }
+}
 
-function check_email() {}
+//Checking password function
+function check_pass(pass_id,passconfirm_id,message_id) {
+  var pass = document.getElementById(pass_id);
+  var passconfirm = document.getElementById(passconfirm_id);
 
-function check_pass() {}
+  if (pass.value != passconfirm.value) {
+    pass.style.borderColor = "red";
+    passconfirm.style.borderColor = "red";
+    document.getElementById(message_id).innerHTML =
+      " كلمة المرور غير متطابقة";
+  } else {
+    if (pass.value == "" && passconfirm.value == "") {
+      pass.style.borderColor = "red";
+      passconfirm.style.borderColor = "red";
+    } else {
+      document.getElementById("passmessage").innerHTML = null;
+      pass.style.borderColor = "#5399B5";
+      passconfirm.style.borderColor = "#5399B5";
+      form = true;
+    }
+  }
+}
 
 //function for edit button in page
 function edit_func() {
   var profile = document.getElementById("profile").elements;
   for (var i = 2; i < profile.length; i++) {
     profile[i].removeAttribute("disabled");
+  }
+}
+function check_form(){
+  if (!form){
+    
   }
 }
