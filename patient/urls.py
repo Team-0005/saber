707c25ef1108e769aae8d7223e46afce099a37a8
Sarah aldumaiji <75248925+saronhkh@users.noreply.github.com>
2023-01-09 18:08:47 +0300

    add conf result

--- patient/urls.py
+++ patient/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<p_email>', views.list_of_patient, name='ptlist'),
     path('addpt/<p_email>', views.addPatient, name='addpt'),
     path('<pt_id>/', views.patientProf , name='patientProfile'),
+    path('confResult/<pt_id>/', views.confResult , name='confResult'),
 ]
