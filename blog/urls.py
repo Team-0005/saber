beca7692f9573b636677071d9ffc941353ae82af
Manar-01 <s439000802@st.uqu.edu.sa>
2023-01-13 01:52:16 +0300

    add buttons to patient profile

--- blog/urls.py
+++ blog/urls.py
@@ -23,13 +23,12 @@ urlpatterns = [
     path("", core_views.home),
     path("admin/", admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
-    path("tests/",include('tests.urls',namespace='tests')),
-    path("patient/",include('patient.urls',namespace='patient')),
+    path("tests/", include('tests.urls', namespace='tests')),
+    path("patient/", include('patient.urls', namespace='patient')),
     path('signup/', core_views.signup, name='signup'),
     path('signin/', core_views.signin, name='signin'),
     path('<p_email>', core_views.profile, name='profile'),
     path('forget/', core_views.forget, name='forget'),
     path('reset/', core_views.reset, name='reset'),
     # path('signout', views.signout, name='signout'),
-]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
-
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
