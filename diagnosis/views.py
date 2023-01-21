110467e7ea96ffbf4d29fbf358752484fc63b1b1
Manar-01 <s439000802@st.uqu.edu.sa>
2023-01-21 04:30:20 +0300

    deploy model

--- diagnosis/views.py
+++ diagnosis/views.py
@@ -1,62 +1,45 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 import numpy as np
 import keras
 from keras.models import load_model
 from blog.models import Psychologist
 
+model_path = 'diagnosis\saber.h5'
+model = load_model(model_path, compile=False)
+disorder = ["desperation","anxiety","OCD"]
+fields=[]
+
 def iniDiag(request):
     try:
-        psycho_email= request.session['psycho_email']
-        print("you are in add patient page")
-        psych = Psychologist.objects.get(p_email=psycho_email)
-        context = {'psycho': psych}
-        if request.method != "POST":
-            return render(request,'diagnosis/iniDiag.html')
-        else: 
-            try:
-                sleep_problems = request.POST['VII3']
-                eating_problems = request.POST['VII1']
-                conscience = request.POST['conscience']
-                communication = request.POST['communication']
-                face_features = request.POST['faceFeatures']
-                talk = request.POST['talk']
-                mood = request.POST['mood']
-                behavior = request.POST['behavior']
-                Fears = request.POST['pt_phone_no']
-                thinking = request.POST['thinking']
-                Focus = request.POST['Focus']
-                Attention = request.POST['Attention']
-                duration = request.POST['pt_gender']
-                fields=[sleep_problems,eating_problems,conscience,communication,face_features,talk,mood,behavior,Fears,thinking,Focus,Attention,duration]
-                if not None in fields:
-                    #Datapreprocessing
-                    fields = list(map(int, fields))
-                    print(fields)
-                    result = np.array(fields).reshape(1, 14) 
-                    #Passing data to model & loading the model from disks
-                    model_path = 'diagnosis\saber.h5'
-                    classifier = keras.load(open(model_path, compile=False))
-                    prediction = classifier.predict([result])[0]
-                    print(prediction)
-                    conf_score =  np.max(classifier.predict_proba([result]))*100
-                    predictions = {
-                        'error' : '0',
-                        'message' : 'Successfull',
-                        'prediction' : prediction,
-                        'confidence_score' : conf_score
-                    }
-                else:
-                    predictions = {
-                        'error' : '1',
-                        'message': 'Invalid Parameters'                
-                    }
-            except Exception as e:
-                predictions = {
-                    'error' : '2',
-                    "message": str(e)
-                }
+        if  request.method == "POST":
+            print("predction start")
+            Datapreprocessing('eating_problems',request.POST['eating_problems'],4)
+            Datapreprocessing('sleep_problems',request.POST['sleep_problems'],3)
+            Datapreprocessing('conscience',request.POST['conscience'],4)
+            Datapreprocessing('communication',request.POST['communication'],3)
+            Datapreprocessing('faceFeatures',request.POST['faceFeatures'],4)
+            Datapreprocessing('talk',request.POST['talk'],4)
+            Datapreprocessing('mood',request.POST['mood'],4)
+            Datapreprocessing('behavior',request.POST['behavior'],4)
+            Datapreprocessing('thinkingObject',request.POST['thinkingObject'],4)
+            Datapreprocessing('thinking',request.POST['thinking'],4)
+            Datapreprocessing('Focus',request.POST['Focus'],3)
+            Datapreprocessing('Attention',request.POST['Attention'],4)
+            Datapreprocessing('orientation',request.POST['orientation'],3)
+            preds = model.predict([fields])[0] 
+            label = disorder[preds.argmax()]
+            print(label)
+            fields.clear()
         return render(request,'diagnosis/iniDiag.html')
     except:
         pass
+        return render(request,'diagnosis/iniDiag.html')
+
+
+
+def Datapreprocessing(option,value,n):
+    for i in range(1,n):
+        if value == (option+"-"+str(i)):
+            fields.append(1)
+        else:
+            fields.append(0)
