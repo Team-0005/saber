84c4b259fcb6653fdcc2d8feb36c13e38c39b00f
Manar-01 <s439000802@st.uqu.edu.sa>
2023-01-23 02:35:52 +0300

    cleaned up patient

--- patient/static/patient/index.js
+++ patient/static/patient/index.js
@@ -1,26 +1,28 @@
 lock = false;
 
-//4
 //force user to enter only number
 function check_number(event) {
   var key = event.keyCode;
   return (key <= 57 && key >= 48);
 }
 
-//5
-//force user to enter only letters
-function check_letter(event) {
+function check_arabic(event) {
   var regularExp = /[\u0600-\u06FF]/;
   var key = event.which;
   var str = String.fromCharCode(key);
-  if(regularExp.test(str)){
+  if (regularExp.test(str)) {
     return true;
   }
   return false;
 };
 
+function check_letter(event) {
+  var key = event.keyCode;
+  return ((key >= 65 && key <= 90) || (key >= 97 && key <= 122));
+}
 
-$('#signin,#signup,#FG_pass,#profile,#patient').submit(function (evt) {
+
+$('#patient').submit(function (evt) {
   if (lock) {
     evt.preventDefault();
   }
@@ -42,5 +44,85 @@ function edit_func() {
 
 //function for edit button in page
 function cancel_func() {
-  location.reload(); 
+  location.reload();
 }
+
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+/*look for any elements with the class "custom-select":*/
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /*for each element, create a new DIV that will act as the selected item:*/
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /*for each element, create a new DIV that will contain the option list:*/
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /*for each option in the original select element,
+    create a new DIV that will act as an option item:*/
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function (e) {
+      /*when an item is clicked, update the original select box,
+      and the selected item:*/
+      var y, i, k, s, h, sl, yl;
+      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+      sl = s.length;
+      h = this.parentNode.previousSibling;
+      for (i = 0; i < sl; i++) {
+        if (s.options[i].innerHTML == this.innerHTML) {
+          s.selectedIndex = i;
+          h.innerHTML = this.innerHTML;
+          y = this.parentNode.getElementsByClassName("same-as-selected");
+          yl = y.length;
+          for (k = 0; k < yl; k++) {
+            y[k].removeAttribute("class");
+          }
+          this.setAttribute("class", "same-as-selected");
+          break;
+        }
+      }
+      h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function (e) {
+    /*when the select box is clicked, close any other select boxes,
+    and open/close the current select box:*/
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+function closeAllSelect(elmnt) {
+  /*a function that will close all select boxes in the document,
+  except the current select box:*/
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+/*if the user clicks anywhere outside the select box,
+then close all select boxes:*/
+document.addEventListener("click", closeAllSelect);
