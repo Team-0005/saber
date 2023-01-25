bf10750a3fed086582169ccf591fe2b2c2836bf3
ReemQaid <reemqaid@outlook.sa>
2023-01-25 20:16:29 +0300

    save the result of patient

--- tests/urls.py
+++ tests/urls.py
@@ -8,9 +8,9 @@ app_name = 'tests'
 urlpatterns = [
     path('', views.test, name='test'),
     path('<int:id>', views.spec, name='spectest'),
-    path('iniDiag/', views.iniDiag, name='iniDiag'),
-    path('test1/', views.test1, name='ocdTest'),
-    path('test2/', views.test2, name='depTest'),
-    path('test3/', views.test3, name='anxTest'),
+    # path('iniDiag/', views.iniDiag, name='iniDiag'),
+    #path('test2/', views.test2, name='ocdTest'),
+    path('test/<int:id>', views.testResult, name='submetTest'),
+    #path('test1/', views.test1, name='anxTest'),
 
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
